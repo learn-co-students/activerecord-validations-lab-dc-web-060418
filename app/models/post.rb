357c7aac43669e87a5ed5cd39164d9d752327fe2
jchamberlain909 <jchamberlain909@gmail.com>
2018-07-02 11:45:11 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,17 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+    validates :content, length: {minimum:250}
+    validates :summary, length: {maximum:250}
+    validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+    validate :my_validator
+
+    def my_validator
+        if title.present?
+            if !(title.include?("Won't Believe") || title.include?("Secret") || title.include?("Guess"))
+                errors.add(:title,"Clickbait")
+            end
+        end
+        
+    end
 end
+
